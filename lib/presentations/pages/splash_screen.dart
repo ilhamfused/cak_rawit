f04bf6dfed94d56b9f50e9260044ef361e5eed33
ilhamfused <irharijanto20@gmail.com>
2025-05-14 08:47:30 +0700

    add splash screen

---
+++ lib/presentations/pages/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:cak_rawit/presentations/pages/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _precacheImages();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return HomeScreen(title: 'Flutter Demo Home Page');
+          },
+        ),
+      );
+    });
+  }
+
+  Future<void> _precacheImages() async {
+    // Preload all the images used in the screen
+    await Future.wait([
+      precacheImage(Image.asset("assets/images/logo-full.png").image, context),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/logo-full.png",
+                width: MediaQuery.of(context).size.width * 0.8,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
